8c461369aa1fbfe4f3ef97fe2306102a9b18a359
agnsm <agnieszka.smolinska.99@gmail.com>
2021-02-08 19:59:23 +0100

    change view

--- CatApp/Startup.cs
+++ CatApp/Startup.cs
@@ -50,7 +50,7 @@ namespace CatApp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Cat}/{action=Index}/{id?}");
             });
         }
     }
--- CatApp/wwwroot/js/site.js
+++ CatApp/wwwroot/js/site.js
@@ -1,4 +1,8 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+$(document).ready(function () {
+    $('.js-tilt').tilt({
+        perspective: 5000,
+        scale: 1.05
+    });
+});
 
-// Write your JavaScript code.
