8e3ed8aa1025af4f4c58afe262fcabcf510fc925
agnsm <agnieszka.smolinska.99@gmail.com>
2021-02-10 17:32:04 +0100

    add css effects in the home view

--- CatApp/wwwroot/js/site.js
+++ CatApp/wwwroot/js/site.js
@@ -1,4 +1,20 @@
 ﻿
+const logo = document.getElementsByClassName("navbar-brand")[0];
+logo.addEventListener("mouseenter", function (e) {
+    e.target.style.letterSpacing = "2px";
+    }, false);
+logo.addEventListener("mouseleave", function (e) {
+    e.target.style.letterSpacing = "";
+}, false);
+
+const catImage = document.getElementsByClassName(".js-tilt")[0];
+logo.addEventListener("mouseenter", function (e) {
+    e.target.style.letterSpacing = "2px";
+}, false);
+logo.addEventListener("mouseleave", function (e) {
+    e.target.style.letterSpacing = "";
+}, false);
+
 $(document).ready(function () {
     $('.js-tilt').tilt({
         perspective: 2000,
